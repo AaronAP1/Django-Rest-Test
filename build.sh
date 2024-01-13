2f659bd54c73a0be44f14d848a3a92a90a38fb49
Aaron <williamsaquinopoma@gmail.com>
2024-01-13 18:41:49 -0500

    cambios security

--- build.sh
+++ build.sh
@@ -8,3 +8,12 @@ pip install -r requerements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
+echo "from django.contrib.auth.models import User; User.objects.create_user('clvadmin', 'aaalenort@gmail.com', 'vivaelbarcelona')" | python manage.py shell
+
+# Puedes agregar más comandos según sea necesario
+
+# Otros comandos que puedas necesitar ejecutar después de la creación del usuario
+# ...
+
+# Finalizar con un mensaje
+echo "¡Script de construcción completado con éxito!"
