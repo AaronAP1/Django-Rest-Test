a8b746ba9b127f74b55d6ccbf393658b01e5554c
Aaron <williamsaquinopoma@gmail.com>
2024-01-13 19:01:12 -0500

    sadasdsa

--- build.sh
+++ build.sh
@@ -8,7 +8,18 @@ pip install -r requerements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-echo "from django.contrib.auth.models import User; User.objects.create_user('clvadmin', 'aaalenort@gmail.com', 'vivaelbarcelona')" | python manage.py shell
+nombre_de_usuario="clv"
+correo_electronico="tu_correo@gmail.com"
+contrasena="vivaelbarcelona124"
+
+# Verificar si el usuario ya existe
+if python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='${nombre_de_usuario}').exists())" | grep "True" &> /dev/null; then
+    echo "El usuario ya existe. No se creará uno nuevo."
+else
+    # Crear un usuario de ejemplo
+    python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('${nombre_de_usuario}', '${correo_electronico}', '${contrasena}')"
+    echo "Usuario creado con éxito."
+fi
 
 # Puedes agregar más comandos según sea necesario
 
