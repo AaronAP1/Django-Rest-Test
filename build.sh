1e71ef70b37bfe1e920b36c66f8c4b9c71deb341
Aaron <williamsaquinopoma@gmail.com>
2024-01-13 19:37:18 -0500

    updatexd

--- build.sh
+++ build.sh
@@ -8,22 +8,22 @@ pip install -r requerements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-nombre_de_usuario="clv"
-correo_electronico="tu_correo@gmail.com"
-contrasena="vivaelbarcelona124"
+nombre_de_usuario="clvsuper"
+correo_electronico="xdxd@ejemplo.com"
+contrasena="contrasegura"
 
-# Verificar si el usuario ya existe
-if python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='${nombre_de_usuario}').exists())" | grep "True" &> /dev/null; then
-    echo "El usuario ya existe. No se creará uno nuevo."
+# Verificar si el superusuario ya existe
+if python manage.py shell -c "from django.contrib.auth.models import User; print(User.objects.filter(username='${nombre_de_usuario}', is_superuser=True).exists())" | grep "True" &> /dev/null; then
+    echo "El superusuario ya existe. No se creará uno nuevo."
 else
-    # Crear un usuario de ejemplo
-    python manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_user('${nombre_de_usuario}', '${correo_electronico}', '${contrasena}')"
-    echo "Usuario creado con éxito."
+    # Crear un superusuario
+    echo "from django.contrib.auth.models import User; User.objects.create_superuser('${nombre_de_usuario}', '${correo_electronico}', '${contrasena}')" | python manage.py shell
+    echo "Superusuario creado con éxito."
 fi
 
 # Puedes agregar más comandos según sea necesario
 
-# Otros comandos que puedas necesitar ejecutar después de la creación del usuario
+# Otros comandos que puedas necesitar ejecutar después de la creación del superusuario
 # ...
 
 # Finalizar con un mensaje
